80ac587dd3152acb761cbda98d397d1ff993bcfe
jerryadepoju <jeremiahadepoju6652@gmail.com>
2024-02-22 07:46:27 +0100

    New imports

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,18 @@
 import Footer from './Components/Footer';
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import * as web3 from '@solana/web3.js';
+import {
+  ConnectionProvider,
+  WalletProvider,
+  useConnection,
+  useWallet,
+  WalletModalProvider,
+  WalletMultiButton,
+} from '@solana/wallet-adapter-react';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import '@solana/wallet-adapter-react-ui/styles.css';
+
 import Home from './Pages/Landing';
 import Landing from './Pages/Home';
 import VotersRegistered from './Pages/VotersRegistered';
@@ -14,59 +26,87 @@ import Register from './Pages/register';
 import ActivatedElections from './Pages/activatedElections';
 
 
+
 function App() {
-  // let message = "SVote is a Solana Blockchain-based Voting System.";
+  const [balance, setBalance] = useState(0);
+  const endpoint = web3.clusterApiUrl('devnet');
+  const wallets = [new PhantomWalletAdapter()];
+
+  const { connection } = useConnection();
+  const { publicKey } = useWallet();
+
+  useEffect(() => {
+    const getInfo = async () => {
+      if (connection && publicKey) {
+        try {
+          const info = await connection.getAccountInfo(publicKey);
+          setBalance(info?.lamports / web3.LAMPORTS_PER_SOL);
+        } catch (error) {
+          console.error('Error getting account info:', error);
+        }
+      }
+    };
+
+    getInfo();
+  }, [connection, publicKey]);
+
+
   const [message, setMessage] = useState('SVote is a Solana Blockchain-based Voting System.');
   const [times, setTimes] = useState('');
-  const link = "http://www.google.com";
   const handleClick = () => {
     setMessage('SVote Rock!');
     setTimes('23 times');
   }
 
   return (
-    <Router>
-      <div className="App">
-        <div className="content">
-          <Switch>
-            <Route exact path='/'>
-              <Home handleClick={handleClick} message={message} link={link} times={times} />
-            </Route>
-            <Route path='/landingpage'>
-              <Landing />
-            </Route>
-            <Route exact path='/votersRegistered'>
-              <VotersRegistered />
-            </Route>
-            <Route path='/votersRegistering'>
-              <VotersRegistering />
-            </Route>
-            <Route path='/createelections'>
-              <CreateElections />
-            </Route>
-            <Route path='/activateelections'>
-              <ActivateElections />
-            </Route>
-            <Route path='/checkresults'>
-              <CheckResults />
-            </Route>
-            <Route path='/publishedresults'>
-              <PublishedResults />
-            </Route>
-            <Route path='/home'>
-              <VoterHome />
-            </Route>
-            <Route path='/register'>
-              <Register />
-            </Route>
-            <Route path='/activatedelections'>
-              <ActivatedElections />
-            </Route>
-          </Switch>        
-        </div>      
-        <Footer></Footer>
-      </div>
-    </Router>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets}>
+        <WalletModalProvider>
+            <Router>
+              <div className="App">
+                <div className="content">
+                  <Switch>
+                    <Route exact path='/'>
+                      <Home handleClick={handleClick} message={message} times={times} />
+                    </Route>
+                    <Route path='/landingpage'>
+                      <Landing />
+                    </Route>
+                    <Route exact path='/votersRegistered'>
+                      <VotersRegistered />
+                    </Route>
+                    <Route path='/votersRegistering'>
+                      <VotersRegistering />
+                    </Route>
+                    <Route path='/createelections'>
+                      <CreateElections />
+                    </Route>
+                    <Route path='/activateelections'>
+                      <ActivateElections />
+                    </Route>
+                    <Route path='/checkresults'>
+                      <CheckResults />
+                    </Route>
+                    <Route path='/publishedresults'>
+                      <PublishedResults />
+                    </Route>
+                    <Route path='/home'>
+                      <VoterHome />
+                    </Route>
+                    <Route path='/register'>
+                      <Register />
+                    </Route>
+                    <Route path='/activatedelections'>
+                      <ActivatedElections />
+                    </Route>
+                  </Switch>        
+                </div>      
+                <Footer></Footer>
+              </div>
+            </Router>
+          </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
     
     
   );
